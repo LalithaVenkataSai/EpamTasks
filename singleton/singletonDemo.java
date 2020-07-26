2705a20d6c980f84488fef729ff772dcfc0c5354
LalithaVenkataSai <60537224+LalithaVenkataSai@users.noreply.github.com>
2020-07-26 23:10:15 +0530

    Delete singletonDemo.java

--- singleton/singletonDemo.java
+++
@@ -1,6 +0,0 @@
-public class singletonDemo {
-    public static void main(String[] args) {
-        SingletonClass singletonClass = SingletonClass.getInstance();
-        singletonClass.showMessage();
-    }
-}
